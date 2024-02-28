88e92d238699081a24c9fa0c5fedf67ab55570bd
Quentin Honnart <quentin.honnart@gmail.com>
2024-02-29 00:47:48 +0100

    run lint fix

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
@@ -10,11 +10,15 @@ export default function App() {
   );
 }
 
+const COLORS = {
+  white: "#FFFFFF"
+};
+
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    backgroundColor: COLORS.white,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: "center"
+  }
 });
