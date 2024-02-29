30ac4949de15194950d819a6e645980b3e833179
Quentin Honnart <quentin.honnart@gmail.com>
2024-02-29 12:09:27 +0100

    Add Safe Area Context

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
+import { SafeAreaView } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthContextProvider } from "~/contexts/AuthContext";
 import Navigator from "~/navigators/Navigator";
 
 export default function App() {
   return (
     <AuthContextProvider>
-      <Navigator />
+      <SafeAreaProvider>
+        <Navigator />
+      </SafeAreaProvider>
     </AuthContextProvider>
   );
 }
