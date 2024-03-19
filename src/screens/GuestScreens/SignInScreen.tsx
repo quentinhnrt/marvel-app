57ec91c73a04a86f474c5b41e6ebd92046d4a640
Quentin <qhonnart@amphibee.fr>
2024-03-19 08:29:16 +0100

    remove sensitive data

---
+++ src/screens/GuestScreens/SignInScreen.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { View } from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { useAuthContext } from "~/contexts/AuthContext";
+
+export default function SignInScreen() {
+  const { signIn, processing, error } = useAuthContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin() {
+    if (processing) {
+      return;
+    }
+
+    signIn({ email, password });
+  }
+
+  return (
+    <SafeAreaView>
+      <View className="px-4 gap-6 justify-center h-screen">
+        <Text
+          variant="headlineLarge"
+          className="text-center my-4 font-bold text-5xl"
+        >
+          Sign In
+        </Text>
+        <Text className="text-center my-4 text-red-500">{error}</Text>
+        <TextInput
+          mode="outlined"
+          placeholder="Type your email"
+          keyboardType="email-address"
+          onChange={(e) => setEmail(e.nativeEvent.text)}
+          value={email}
+        />
+        <TextInput
+          mode="outlined"
+          placeholder="Type your password"
+          secureTextEntry
+          onChange={(e) => setPassword(e.nativeEvent.text)}
+          value={password}
+        />
+        <Button
+          mode="contained"
+          onPress={handleLogin}
+        >
+          Login
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
+}
